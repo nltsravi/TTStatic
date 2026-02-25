47c27c4f42ac3218b47508dc50545c88d2f64d6d
nltsravi <ravi@neolumina.in>
2026-02-25 20:23:44 +0530

    add github actions workflow and configure basepath

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = "";
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  repo = `/${process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')}`;
+}
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  reactCompiler: true,
+  basePath: repo || '',
+  assetPrefix: repo || '',
 };
 
 export default nextConfig;
